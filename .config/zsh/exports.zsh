2f0684a2563825e58ab56ef77562c85e093ef3e1
Joaquin Flores <joaquin_fp2000@hotmail.com>
2020-08-12 18:50:35 -0500

    Minor fixes

--- .config/zsh/exports.zsh
+++ .config/zsh/exports.zsh
@@ -1,7 +1,7 @@
-EDITOR="nvim"
-export TERM="xterm-256color"
+export EDITOR=nvim
+export TERM=alacritty
 
-# Some minor fixes
+# Some minor exports
 export EDITOR=nvim
 export HISTCONTROL=ignoreboth
 
@@ -15,6 +15,10 @@ export GOPATH=$GOPATH:$HOME/gowork
 #VERSION='v12.18.3'
 #DISTRO='linux-x64'
 #NODEJS="/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/"
+
+# MAN
+export MANPATH=/usr/share/man
+
 
 # PATH -- configuracion base y despues añade extras
 PATH="/home/joako/bin:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl"
