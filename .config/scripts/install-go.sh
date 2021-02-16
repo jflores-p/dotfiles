69545896e6670c4ea9890c5aecb77f7ed604970b
Joaquin Flores <joaquin_fp2000@hotmail.com>
2021-02-16 15:10:25 -0500

    Cambios menores

--- .config/scripts/install-go.sh
+++ .config/scripts/install-go.sh
@@ -6,19 +6,20 @@ cd $HOME/Downloads
 declare GO_VER=$1
 declare GO_DIR="/usr/local/go/${GO_VER}"
 
-if [ ! -d ${GO_DIR} ]; then
-    echo "Creando ${GO_DIR}"
-    sudo mkdir -p ${GO_DIR}
-fi
+# if [ ! -d ${GO_DIR} ]; then
+    # echo "Creando ${GO_DIR}"
+    # sudo mkdir -p ${GO_DIR}
+# fi
 
 if [ "$(/usr/bin/ls -A ${GO_DIR})" ]; then
     echo "Golang version ${GO_VER} ya se encuentra instalada"
     
 else
-    # wget "https://golang.org/dl/go${GO_VER}.linux-amd64.tar.gz"
+    wget "https://golang.org/dl/go${GO_VER}.linux-amd64.tar.gz"
 
     sudo tar -C ${GO_DIR} -xzf "go${GO_VER}.linux-amd64.tar.gz"
     
+    sudo mv "go/" "${GO_DIR}"
     sudo rm "go${GO_VER}.linux-amd64.tar.gz"
 fi
 
